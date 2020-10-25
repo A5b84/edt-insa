3fdaae8f8e22cf7851af680c134e48b77a20bd18
A5b84 <42934711+A5b84@users.noreply.github.com>
2020-10-25 16:19:50 +0100

    Nettoyage main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import * as pako from 'pako';
+import Pako from 'pako';
 import Calendar from './Calendar';
 import { parseIcal } from './parseIcal';
 import { addSwipeListener } from './Utils';
@@ -26,71 +26,6 @@ const search = new URLSearchParams(location.search);
 const calendarId = search.get('cal');
 const cacheKey = `adeCache_${calendarId}`;
 const cacheTimeKey = `adeCacheTime_${calendarId}`;
-
-
-
-function loadIcal(ical: string): void {
-    calendar.setEvents(parseIcal(ical));
-
-    // Date de récupération
-    const cacheTime: number = +localStorage[cacheTimeKey];
-    if (isFinite(cacheTime)) { // Mieux que isNaN (pas d'infini)
-        const d = new Date(cacheTime);
-        const date = d.toLocaleDateString('fr', { month: 'short', day: 'numeric' });
-        const time = d.toLocaleTimeString('fr', { hour: 'numeric', minute: 'numeric' });
-        fetchDateEl.innerText = `Récupéré le ${date} à\u00a0${time}`;
-    }
-}
-
-function fetchIcal(): void {
-    forceRefreshBtn.disabled = true;
-    document.body.classList.add('fetching');
-
-    fetch(`https://cors-anywhere.herokuapp.com/https://ade-outils.insa-lyon.fr/ADE-Cal:~${calendarId}`)
-    .then(response => response.text())
-    .then(ical => {
-        localStorage[cacheTimeKey] = Date.now();
-        loadIcal(ical);
-        localStorage[cacheKey] = pako.deflate(ical, { to: 'string' });
-    })
-    .finally(() => {
-        forceRefreshBtn.disabled = false;
-        document.body.classList.remove('fetching');
-    });
-}
-
-
-
-/** Met la date du calendrier dans `dateInput` */
-function updateInputDate(): void {
-    // On peut pas juste faire 'dateInput.valueAsDate = calendar.getDate()'
-    // parce que ça utilise pas le fuseau horaire donc ça peut être décalé
-    const d = calendar.getDate();
-    var month = d.getMonth() + 1 + '';
-    var day = d.getDate() + '';
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
-    dateInput.value = `${d.getFullYear()}-${month}-${day}`;
-}
-
-function setDate(date: Date): void {
-    dateInput.valueAsDate = date; // Convertit les dates invalide en null
-    if (dateInput.valueAsDate) {
-        calendar.setDate(dateInput.valueAsDate);
-    } else {
-        calendar.setDate(new Date());
-        updateInputDate();
-    }
-}
-
-
-
-function timeButtonHandler(offset: -1 | 1): void {
-    if (offset > 0) calendar.moveToNext();
-    else calendar.moveToPrevious();
-
-    updateInputDate();
-}
 
 
 
@@ -171,7 +106,7 @@ if (!calendarId) {
 // Chargement du calendrier dans le cache
 if (localStorage[cacheKey]) {
     try {
-        loadIcal(pako.inflate(localStorage[cacheKey], { to: 'string' }));
+        loadIcal(Pako.inflate(localStorage[cacheKey], { to: 'string' }));
     } catch (e) {
         // Au cas où
         console.error(e);
@@ -184,4 +119,73 @@ if (!localStorage[cacheKey]
         || !localStorage[cacheTimeKey]
         || Date.now() - +localStorage[cacheTimeKey] >= MAX_CACHE_AGE) {
     fetchIcal();
+}
+
+
+
+/** Charge un calendrier à partir du contenu d'un fichier iCalendar */
+function loadIcal(ical: string): void {
+    calendar.setEvents(parseIcal(ical));
+
+    // Date de récupération
+    const cacheTime: number = +localStorage[cacheTimeKey];
+    if (isFinite(cacheTime)) { // Mieux que isNaN (pas d'infini)
+        const d = new Date(cacheTime);
+        const date = d.toLocaleDateString('fr', { month: 'short', day: 'numeric' });
+        const time = d.toLocaleTimeString('fr', { hour: 'numeric', minute: 'numeric' });
+        fetchDateEl.innerText = `Récupéré le ${date} à\u00a0${time}`;
+    }
+}
+
+/** Récupère et charge le calendrier correspondant à `calendarId` */
+function fetchIcal(): void {
+    forceRefreshBtn.disabled = true;
+    document.body.classList.add('fetching');
+
+    fetch(`https://cors-anywhere.herokuapp.com/https://ade-outils.insa-lyon.fr/ADE-Cal:~${calendarId}`)
+    .then(response => response.text())
+    .then(ical => {
+        localStorage[cacheTimeKey] = Date.now();
+        loadIcal(ical);
+        localStorage[cacheKey] = Pako.deflate(ical, { to: 'string' });
+    })
+    .finally(() => {
+        forceRefreshBtn.disabled = false;
+        document.body.classList.remove('fetching');
+    });
+}
+
+
+
+/** Met la date du calendrier dans `dateInput` */
+function updateInputDate(): void {
+    // On peut pas juste faire 'dateInput.valueAsDate = calendar.getDate()'
+    // parce que ça utilise pas le fuseau horaire donc ça peut être décalé
+    const d = calendar.getDate();
+    var month = d.getMonth() + 1 + '';
+    var day = d.getDate() + '';
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+    dateInput.value = `${d.getFullYear()}-${month}-${day}`;
+}
+
+/** Change la date affichée et du calendrier */
+function setDate(date: Date): void {
+    dateInput.valueAsDate = date; // Convertit les dates invalide en null
+    if (dateInput.valueAsDate) {
+        calendar.setDate(dateInput.valueAsDate);
+    } else {
+        calendar.setDate(new Date());
+        updateInputDate();
+    }
+}
+
+
+
+/** Avance/recule d'un jour / une semaine */
+function timeButtonHandler(offset: -1 | 1): void {
+    if (offset > 0) calendar.moveToNext();
+    else calendar.moveToPrevious();
+
+    updateInputDate();
 }
